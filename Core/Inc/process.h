9c3f1f6df214273c00c55499d99ec6dfd365d448
Hubert Droździak <hubertdrozdziak@gmail.com>
2023-06-19 10:57:33 +0200

    added processing files

---
+++ Core/Inc/process.h
@@ -0,0 +1,16 @@
+#ifndef __PROCESS_H__
+#define __PROCESS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ PROCESS_H__ */
