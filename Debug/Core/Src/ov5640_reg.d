35a59c342d228b559a74988aba084c14557dcdba
Hubert Droździak <hubertdrozdziak@gmail.com>
2023-06-26 14:37:25 +0200

    delete unnecessary files

--- Debug/Core/Src/ov5640_reg.d
+++
@@ -1,6 +0,0 @@
-Core/Src/ov5640_reg.o: ../Core/Src/ov5640_reg.c ../Core/Inc/ov5640_reg.h \
- ../Drivers/CMSIS/Include/cmsis_compiler.h \
- ../Drivers/CMSIS/Include/cmsis_gcc.h
-../Core/Inc/ov5640_reg.h:
-../Drivers/CMSIS/Include/cmsis_compiler.h:
-../Drivers/CMSIS/Include/cmsis_gcc.h:
